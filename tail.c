56bc3c77a39c930a7f1458391e7121bb2d69ae10
jakubKuznik <kuznja16@gmail.com>
2021-03-27 14:43:43 +0100

    Error handlings in parser done.

--- tail.c
+++ tail.c
@@ -18,6 +18,9 @@
 #define IS_THERE_A_FILE_POSITION 1
 #define FILE_NAME_START_BYTE 2
 
+#define MAX_ROWS_DIGIT 12 // 1000 = 10 * 10^12
+#define INT_MAX 2147483647
+
 int main(int argc, char *argv[])
 {
 
@@ -36,9 +39,13 @@ int main(int argc, char *argv[])
         n = 10;
     }
 
-    for(int i = 0; i < 258; i++)
-        printf("%c",arguments[i]);
-    
+    //TODO check if file exist function 
+    if(arguments[IS_THERE_A_FILE_POSITION] == 1)
+    {
+        printf("file entred. ");
+    }
+
+    printf("%d",n);
 
     return 0;
 }
@@ -56,12 +63,15 @@ int main(int argc, char *argv[])
  */
 int arg_parser(int argc, char *argv[], char arguments[])
 {
+    int n = 10;
+    char help[MAX_ROWS_DIGIT] = {0}; //tempo variable where number will be stored 
     
     if(argc == 1) // No arguments
         return -2;
 
-
-    for(int i=1; i < argc ;i++)
+    // Go throw all the argv[] loking for:
+    // -n [+]number file 
+    for(int i=1; i < argc ;i++) //for -n [+]number
     {		
         if(strcmp(argv[i], "-n")==0)
         {
@@ -70,23 +80,95 @@ int arg_parser(int argc, char *argv[], char arguments[])
                 if(argv[i+1][0] == '+')
                 {
                     arguments[SIGN_INDICATOR_POSITION] = 1;
-                    for(int j = 1; argv[i+1][j] != '\0'; j++)
-                        arguments[FILE_NAME_START_BYTE + (j - 1)] = argv[i+1][j];
+                    if(copy_from_to(argv, help, i+1, MAX_ROWS_DIGIT ,1 , 0) == -1)
+                        goto error_4;
                 }
                 else
                 {
-                    for(int j = 0; argv[i+1][j] != '\0'; j++)
-                        arguments[FILE_NAME_START_BYTE + j] = argv[i+1][j];
+                    if(copy_from_to(argv, help, i+1, MAX_ROWS_DIGIT, 0 , 0) == -1)
+                        goto error_4;
                 }
             }
             else
-            {
-                fprintf(stderr, "Error missing number after -n argument.\n");
-                return -1;
-            }
+                goto error_2;
+            i++;
+            continue;
+        }
+        else //for file 
+        {
+            if(arguments[IS_THERE_A_FILE_POSITION] == 1) //multiple file names 
+                goto error_3;
+            
+            arguments[IS_THERE_A_FILE_POSITION] = 1;
+            if(copy_from_to(argv, arguments, i, FILENAME_MAX, FILE_NAME_START_BYTE, 0) == -1)
+                goto error_5;
         }
     }
 
+    //check if number in (-n number) is number.
+    if(check_if_array_is_digit(help, MAX_ROWS_DIGIT, arguments[SIGN_INDICATOR_POSITION]) == -1)
+        goto error_1;
+
+    if(atoi(help) > INT_MAX || atoi(help) < 0)
+        goto error_4;
+
+    n = atoi(help);
+
+
+    return n;
+
+
+//////////////ERRORS 
+error_1:
+    fprintf(stderr, "Error there has to be digit after -n.\n");
+    return -1;
+
+error_2:
+    fprintf(stderr, "Error missing number after -n argument.\n");
+    return -1;
+
+error_3:
+    fprintf(stderr, "Error there can be only one file.\n");
+    return -1;
+
+error_4:
+    fprintf(stderr, "Number has to be positive. Or number is to big.\n");
+    return -1;
+
+error_5:
+    fprintf(stderr, "Error to big file name \n");
+    return -1;
+}
+
+/**
+ * Just copy from to 
+ * from[][] is 
+ * return -1 if limit was reached
+ */
+int copy_from_to(char *from[], char to[], int first_index, int limit, int from_start_byte, int to_start_byte)
+{
+    for(int j = from_start_byte; (from[first_index][j] != '\0'); j++)
+    {
+        if(j > limit)
+            return -1;
+        to[to_start_byte + j] = from[first_index][j];
+    }
     return 0;
 }
 
+/**
+ * Check if array of given size has just number
+ * return -1 if error 
+ */
+int check_if_array_is_digit(char array[], int size, int plus_sign)
+{
+    int j = 0;
+    if(plus_sign = 1)
+        j++;
+
+    for(; (j < size) && (array[j] != '\0'); j++)
+        if(!isdigit(array[j]))
+            return -1;
+    return 0;
+
+}
